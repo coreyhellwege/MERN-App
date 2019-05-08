62ef0c59cca33c3865971ed36b5949e25bea98c0
Corey Hellwege <coreyhellwege@gmail.com>
2019-05-08 12:17:55 +1000

    create Post model

---
+++ models/Post.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; // assign mongoose.Schema to it's own variable for shorter syntax
+
+const PostSchema = new Schema({
+  // posts should be associated with a user
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users" // reference user model
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    // username (not title of post)
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    // for the post
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model("post, PostSchema");
