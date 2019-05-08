1beb8ac6065244d16e9b5315e95a0cfe4236170d
Corey Hellwege <coreyhellwege@gmail.com>
2019-05-08 16:01:32 +1000

    add a post route for the Post model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,51 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+const auth = require("../../middleware/auth");
+
+// Import models
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // Route:         GET api/posts
-// Description:   Test route
-// Access:        Public
-router.get("/", (req, res) => {
-  console.log(req.body);
-  res.send("Posts route");
-});
+// Description:   Create a post
+// Access:        Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() }); // errors.array is a method available to us from the errors object
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password"); // don't include password
+
+      // Setup an object for a new post
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
