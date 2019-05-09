dfb74d991f1ca8a84c6acc955e600abe64724a8d
Corey Hellwege <coreyhellwege@gmail.com>
2019-05-09 13:58:27 +1000

    created login and sign up forms

--- backend/frontend/src/components/auth/Login.js
+++ backend/frontend/src/components/auth/Login.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  // formData is the state (which is an object with all the form data)
+  // setFormData is the function used to update the state
+  // useState() is a hook
+  const [formData, setFormData] = useState({
+    // default values for initial state
+    email: "",
+    password: ""
+  });
+
+  // destructure for shorter syntax to access values from formData
+  const { email, password } = formData;
+
+  const onChange = e =>
+    // set name to the value of the input
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  // the spread operator '...' copies the contents of the object
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log("SUCCESS");
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Sign In to your Account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            minLength="6"
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
--- backend/frontend/src/components/auth/Register.js
+++ backend/frontend/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   // formData is the state (which is an object with all the form data)
@@ -87,7 +88,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
