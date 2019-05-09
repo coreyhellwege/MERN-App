de4f38b7f0c35955030242129f2e16831cda12d4
Corey Hellwege <coreyhellwege@gmail.com>
2019-05-09 10:43:58 +1000

    setup react and concurrently, restructured git

---
+++ backend/models/User.js
@@ -0,0 +1,31 @@
+// Naming convention for models is singular and to start with a capital letter
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create User Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true // same email address can't be registered twice
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
