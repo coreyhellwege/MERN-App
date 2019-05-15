921618d670195506b1238b592111d80567acb755
Corey Hellwege <coreyhellwege@gmail.com>
2019-05-15 15:37:12 +1000

    added get profile actions and reducers

--- backend/frontend/src/reducers/profile.js
+++ backend/frontend/src/reducers/profile.js
@@ -1,8 +1,10 @@
 import {
   GET_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
   UPDATE_PROFILE,
-  PROFILE_ERROR,
-  CLEAR_PROFILE
+  CLEAR_PROFILE,
+  PROFILE_ERROR
 } from "../actions/types";
 
 const initialState = {
@@ -25,6 +27,12 @@ export default function(state = initialState, action) {
         profile: payload, // set the profile to the profile data sent back through the payload
         laoding: false // set to false once loading is complete
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state,
@@ -36,6 +44,12 @@ export default function(state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
+        loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false
       };
     default:
