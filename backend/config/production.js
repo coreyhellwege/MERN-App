e6b558cfc215cde92f6fb492460871be11761498
Corey Hellwege <coreyhellwege@gmail.com>
2019-05-24 12:12:01 +1000

    prepare for second deploy

---
+++ backend/config/production.js
@@ -0,0 +1,4 @@
+export const mongoURI = process.env.MONGO_URI;
+export const jwtSecret = process.env.JWT_SECRET;
+export const githubClientId = process.env.GITHUB_CLIENT_ID;
+export const githubSecret = process.env.GITHUB_SECRET;
