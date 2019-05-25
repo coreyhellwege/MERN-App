516e75bd547388b983feb43d44150059c92c28a1
Corey Hellwege <coreyhellwege@gmail.com>
2019-05-25 17:20:26 +1000

    server port fix

--- server.js
+++ server.js
@@ -26,10 +26,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
